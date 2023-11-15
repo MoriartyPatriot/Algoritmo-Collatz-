3889e714b95621494bbd88ce2eb125abef313e3e
MoriartyPatriot <92971793+MoriartyPatriot@users.noreply.github.com>
2023-11-15 09:33:27 -0500

    collatz

---
+++ collatz.py
@@ -0,0 +1,22 @@
+def Collatz(n):
+    print("primer termino de la secesion:", n)
+    while(n != 0):
+       if n = int(n/2)
+        print(n)
+         elif(n % 2 ! = 0 ):
+             n = 3*n + 1
+             print(n)
+            
+            
+            
+suma = 0
+for i in range(5):
+    i = int(input("ingrese n menor a 258"))
+    suma = suma + i
+    
+N = int(suma / 5)
+
+
+
+Collatz(N)
+            
